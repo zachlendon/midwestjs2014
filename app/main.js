badc3696758f38154c44c3984f0c885f785bd200
Ryan Florence <rpflorence@gmail.com>
2014-06-19 14:03:16 -0600

    remove redundancy

--- app/main.js
+++ app/main.js
@@ -4,18 +4,13 @@ var RNR = require('react-nested-router');
 var Router = RNR.Router;
 var Route = RNR.Route;
 
-var App = require('./components/app');
-var Index = require('./components/index');
-var Foo = require('./components/foo');
-var Bar = require('./components/bar');
-
 Router.useHistory();
 
 Router(
-  <Route handler={App}>
-    <Route name="foo" handler={Foo} />
-    <Route name="bar" path="/what/evz" handler={Bar} />
-    <Route name="index" path="/" handler={Index} />
+  <Route handler={require('./components/app')}>
+    <Route name="foo" handler={require('./components/foo')} />
+    <Route name="bar" path="/what/evz" handler={require('./components/bar')} />
+    <Route name="index" path="/" handler={require('./components/index')} />
   </Route>
 ).renderComponent(document.body);
 
